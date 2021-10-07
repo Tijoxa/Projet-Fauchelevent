b89785aaf10d8fb894ed922cf46c9d77155cfe58
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2021-10-07 18:50:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,9 @@ from torch.utils.data.sampler import SequentialSampler
 from matplotlib import pyplot as plt
 
 ## Open and process files
-path = '/home/ankama/MEDIGATE/code/'
+path = 'C:/Users/TM/Documents/Files/ML/fauchlevent/code/'
 
 WEIGHTS_FILE = path+'fasterrcnn_resnet50_fpn_best.pth'
-
 train_df = pd.read_csv(path+'train.csv')
 submit = pd.read_csv(path+'sample_submission.csv')
 
@@ -44,17 +43,10 @@ train_df['y'] = train_df['y'].astype(np.float)
 train_df['w'] = train_df['w'].astype(np.float)
 train_df['h'] = train_df['h'].astype(np.float)
 
-## Split the data into train and validation set
 image_ids = train_df['image_id'].unique()
-valid_ids = image_ids[-665:] # 665 images for valid
-train_ids = image_ids[:-665] # 2 708 images for training
-
-valid_df = train_df[train_df['image_id'].isin(valid_ids)]
-train_df = train_df[train_df['image_id'].isin(train_ids)]
-
 trans = transforms.Compose([transforms.ToTensor()])
 
-## Write a custom dataset
+## Write a custom dataset (TO DO: support multiple classes)
 class WheatDataset(Dataset):
 
     def __init__(self, dataframe, image_dir, transforms = None, train = True):
@@ -130,28 +122,18 @@ class Averager:      ##Return the average loss
         self.current_total = 0.0
         self.iterations = 0.0
 
-## Create stuff
+## Create model entries
 def collate_fn(batch):
     return tuple(zip(*batch))
 
 train_dataset = WheatDataset(train_df, train_dir, trans, True)
-valid_dataset = WheatDataset(valid_df, train_dir, trans, True)
-
 
 # Split the dataset in train and test set
 indices = torch.randperm(len(train_dataset)).tolist()
 
 train_data_loader = DataLoader(
-    train_dataset,
+    train_df,
     batch_size = 16,
-    shuffle = False,
-    num_workers = 4,
-    collate_fn = collate_fn
-)
-
-valid_data_loader = DataLoader(
-    valid_dataset,
-    batch_size = 8,
     shuffle = False,
     num_workers = 4,
     collate_fn = collate_fn
@@ -173,23 +155,24 @@ model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 model.load_state_dict(torch.load(WEIGHTS_FILE, map_location = device))
 
 ## Train the model
+"""
 model.train()
 model.to(device)
 params = [p for p in model.parameters() if p.requires_grad]
 optimizer = torch.optim.SGD(params, lr = 0.03, momentum = 0.9, weight_decay = 0.00001)
 lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 1, gamma = 0.5)
 
-num_epochs = 3
+num_epochs = 4
 
 loss_hist = Averager()
-itr = 1
 
 for epoch in range(num_epochs):
     loss_hist.reset()
 
     for i in range(len(train_data_loader.dataset)):
-        image = [train_data_loader.dataset[i][0]]
-        target = [train_data_loader.dataset[i][1]]
+        image = [train_data_loader.dataset[i][0].to(device)]
+        target = [{'boxes': train_data_loader.dataset[i][1]['boxes'].to(device), 'labels': train_data_loader.dataset[i][1]['labels'].to(device), 'iscrowd': train_data_loader.dataset[i][1]['iscrowd'].to(device), 'area': train_data_loader.dataset[i][1]['area'].to(device), 'image_id': train_data_loader.dataset[i][1]['image_id'].to(device)}]
+
         loss_dict = model(image, target)   # Return the loss
         losses = sum(loss for loss in loss_dict.values())
         loss_value = losses.item()
@@ -202,13 +185,21 @@ for epoch in range(num_epochs):
     if lr_scheduler is not None:
         lr_scheduler.step()
 
-    print(f"Epoch #{epoch} loss: {loss_hist.value}")
+    print(f"Epoch #{epoch + 1} loss: {loss_hist.value}")
 
-## Prediction
-test_dataset = WheatDataset(submit, test_dir, trans, False)
+torch.save(model, path + "model_saved")
+"""
+
+## Load the model
+model = torch.load(path + "model_saved", map_location = 'cpu')
+
+## Prediction (TO DO: support prediction on GPU)
+detection_threshold = 0.5
+
+trans = transforms.Compose([transforms.ToTensor()])
+test_dataset = WheatDataset(submit, test_dir, trans, train = False)
 test_data_loader = DataLoader(test_dataset, batch_size = 8, shuffle = False)
 
-detection_threshold = 0.45
 def format_prediction_string(boxes, scores): ## Define the formate for storing prediction results
     pred_strings = []
     for j in zip(scores, boxes):
@@ -216,25 +207,22 @@ def format_prediction_string(boxes, scores): ## Define the formate for storing p
 
     return " ".join(pred_strings)
 
-results = []
+outputs, results = [], []
 model.eval()
 
-for images, image_ids in test_data_loader:
+for image, image_id in test_data_loader.dataset:  # Prediction is done image by image because of memory concerns
+    outputs += model([image])
 
-    images = list(image.to(device) for image in images)
-    outputs = model(images)
-
-    for i, image in enumerate(images):
-
+    for i, image in enumerate([image]):
         boxes = outputs[i]['boxes'].data.cpu().numpy()  # Formate of the output's box is [Xmin, Ymin, Xmax, Ymax]
         scores = outputs[i]['scores'].data.cpu().numpy()
 
         boxes = boxes[scores >= detection_threshold].astype(np.int32)  # Compare the score of output with the threshold and
         scores = scores[scores >= detection_threshold]  # Select only those boxes whose score is greater than threshold value
-        image_id = image_ids[i]
+        image_id = image_id[i]
 
         boxes[:, 2] = boxes[:, 2]-boxes[:, 0]
-        boxes[:, 3] = boxes[:, 3]-boxes[:, 1]  # Convert the box formate to [Xmin,Ymin,W,H]
+        boxes[:, 3] = boxes[:, 3]-boxes[:, 1]  # Convert the box formate to [Xmin, Ymin, W, H]
 
         result = {  # Store the image id and boxes and scores in result dict.
             'image_id': image_id,
@@ -245,17 +233,40 @@ for images, image_ids in test_data_loader:
 
 test_df = pd.DataFrame(results, columns=['image_id', 'PredictionString'])
 
-## Display prediction of first image
-sample = images[0].permute(1,2,0).cpu().numpy()
-boxes = outputs[0]['boxes'].data.cpu().numpy()
-boxes = boxes[scores >= detection_threshold].astype(np.int32)
-scores = outputs[0]['scores'].data.cpu().numpy()
-fig, ax = plt.subplots(1, 1, figsize=(16, 8))
-for box in boxes :
-    sample = cv2.rectangle(sample, (box[0], box[1]), (box[2], box[3]), (220, 0, 0), 2)
+## Display prediction of image IMG (TO DO: fix the issue of colors)
+IMG = 9
 
-ax.set_axis_off()
-ax.imshow(sample)
+sample = test_data_loader.dataset[IMG][0].permute(1,2,0).cpu().numpy()
+for i in range(len(sample)):
+    for j in range(len(sample[i])):
+        sample[i][j][0] = 255*sample[i][j][0]
+        sample[i][j][1] = 255*sample[i][j][1]
+        sample[i][j][2] = 255*sample[i][j][2]
+
+scores = outputs[IMG]['scores'].data.cpu().numpy()
+boxes = outputs[IMG]['boxes'].data.cpu().numpy()
+boxes = boxes[scores >= detection_threshold].astype(np.int32)
+
+def rectangle(npimage, xmin, ymin, xmax, ymax, color = [0, 0, 0]):  # npimage in format (x, y, 3 colors)
+    xmax = min(xmax, len(npimage)-1)
+    ymax = min(ymax, len(npimage[0])-1)
+
+    for i in range(xmin, xmax):
+        npimage[i][ymin] = color
+        npimage[i][ymax] = color
+
+    for j in range(ymin, ymax):
+        npimage[xmin][j] = color
+        npimage[xmax][j] = color
+
+    return npimage
+
+for box in boxes :
+    sample = rectangle(sample, box[0], box[1], box[2], box[3], [220, 0, 0])
+
+sampleint = sample.astype(np.int32)
+
+cv2.imwrite(path + 'output.png', sampleint)
 
 ## Submission
 test_df.to_csv('submission.csv', index = False)
