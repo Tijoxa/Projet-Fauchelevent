ba24aca57cd66089d34605ffd03f00bb7e054d71
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2021-10-22 15:27:31 +0200

    Update imageai prediction classes.py

--- ImageAI37/imageai prediction classes.py
+++ ImageAI37/imageai prediction classes.py
@@ -3,7 +3,7 @@ from imageai.Classification.Custom import ClassificationModelTrainer
 
 model_trainer = ClassificationModelTrainer()
 model_trainer.setModelTypeAsMobileNetV2()
-model_trainer.setDataDirectory(r"C:/Users/DL/Documents/code/dataset Global Wheat", json_subdirectory = r"C:/Users/DL/Documents/code/project with ImageAI")
+model_trainer.setDataDirectory(r"C:/Users/DL/Documents/code/dataset Global Wheat", json_subdirectory = r"C:/Users/DL/Documents/code/project with ImageAI", train_subdirectory = "C:/Users/DL/Documents/code/dataset Global Wheat/train balanced a la zeub")
 
 ##
 model_trainer.trainModel(num_objects=4, num_experiments=50, enhance_data=True, batch_size=4, training_image_size=1024)
@@ -13,7 +13,7 @@ from imageai.Classification.Custom import CustomImageClassification
 
 prediction = CustomImageClassification()
 prediction.setModelTypeAsMobileNetV2()
-prediction.setModelPath("C:/Users/DL/Documents/code/dataset Global Wheat\models\model_ex-038_acc-0.781022.h5")
+prediction.setModelPath("C:/Users/DL/Documents/code/dataset Global Wheat/models/model_ex-038_acc-0.781022.h5")
 prediction.setJsonPath("C:/Users/DL/Documents/code/project with ImageAI/model_class.json")
 prediction.loadModel(num_objects=4)
 
@@ -24,4 +24,22 @@ print(predictions, probabilities)
 
 # Résultat : dataset pas équilibré, donc prédit Epi tout le temps
 
-## Data augmentation to balance dataset
+## Data augmentation to balance dataset  tf.keras.preprocessing.image.ImageDataGenerator
+import os
+
+maxLengthLabel = 0
+
+root='C:/Users/DL/Documents/code/dataset Global Wheat/train/'
+dirlist = [ item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item)) ]
+
+for i in range(len(dirlist)):
+    folderName = "C:/Users/DL/Documents/code/dataset Global Wheat/train/" + dirlist[i]+"/"
+    maxLengthLabel = max(maxLengthLabel, len([name for name in os.listdir(folderName)]))
+
+for i in range(len(dirlist)):
+    folderName = "C:/Users/DL/Documents/code/dataset Global Wheat/train/" + dirlist[i]+"/"
+
+    while (len([name for name in os.listdir(folderName)]) < maxLengthLabel):
+        createRandomImage("truc")  # fonction à écrire
+
+##
