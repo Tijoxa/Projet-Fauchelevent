d0794379b44e4436edff1c2f930226eab374c584
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2021-10-25 15:25:13 +0200

    Add files via upload

---
+++ ImageAI/main.py
@@ -0,0 +1,45 @@
+path = "C:/Users/DL/Documents/code/GitHub/ImageAI/"
+
+import sys
+sys.path.append(path)
+
+## Train
+from imageaicustom import ClassificationModelTrainer
+
+model_trainer = ClassificationModelTrainer()
+model_trainer.setModelTypeAsMobileNetV2()
+model_trainer.setDataDirectory(path + "dataset", json_subdirectory = path, test_subdirectory = path + "dataset/validation")
+
+## class_weigth_custom
+import os
+
+dirlist = [item for item in os.listdir(path + "dataset/train/") if os.path.isdir(os.path.join(path + "dataset/train/", item))]
+
+lengthLabel = [1.0 for i in range(len(dirlist))]
+
+for i in range(len(dirlist)):
+    folderName = path + "dataset/train/" + dirlist[i]+"/"
+    lengthLabel[i] = len([name for name in os.listdir(folderName)])
+
+maxLengthLabel = max(lengthLabel)
+
+class_weight_custom = {0: maxLengthLabel/lengthLabel[0], 1: maxLengthLabel/lengthLabel[1], 2: maxLengthLabel/lengthLabel[2], 3: maxLengthLabel/lengthLabel[3]}  # the json file provides the mapping between the folder name and the number of its class
+
+##
+model_trainer.trainModel(num_objects=4, num_experiments=1, enhance_data=True, batch_size=4, training_image_size=1024, class_weight_custom=class_weight_custom)
+
+## Test
+from imageaicustom import CustomImageClassification
+
+prediction = CustomImageClassification()
+prediction.setModelTypeAsMobileNetV2()
+prediction.setModelPath(path + "dataset/models/model_ex-013_acc-0.510417.h5")
+prediction.setJsonPath(path +"model_class.json")
+prediction.loadModel(num_objects=4)
+
+##
+file_name = "0eee92ea416f01ff18578463a9ba9014dca90e8c33dde3f1f59592d52d082cab.png"
+
+predictions, probabilities = prediction.classifyImage(path + "dataset/test/" + file_name, result_count=4)
+
+print(predictions, probabilities)
---
+++ ImageAI/requirements.py
@@ -0,0 +1,12 @@
+from pip._internal import main as pipmain
+
+pipmain(['install', 'tensorflow==2.4.0'])
+pipmain(['install', 'tensorflow-gpu==2.4.0'])
+pipmain(['install', 'keras==2.4.3'])
+pipmain(['install', 'numpy==1.19.3'])
+pipmain(['install', 'pillow==7.0.0'])
+pipmain(['install', 'scipy==1.4.1'])
+pipmain(['install', 'h5py==2.10.0'])
+pipmain(['install', 'matplotlib==3.3.2'])
+pipmain(['install', 'opencv-python'])
+pipmain(['install', 'keras-resnet==0.2.0'])
