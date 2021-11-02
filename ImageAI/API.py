6afd34d140d4e9cb6eeb803d3189126d005708d1
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2021-11-02 18:05:16 +0100

    Update API.py

--- ImageAI/API.py
+++ ImageAI/API.py
@@ -47,7 +47,7 @@ def loadFiles():
 def nextImage(number_image):
     img = Image.open(files[number_image])
     tk_img = ImageTk.PhotoImage(img)
-    label_image = Label(frame_prediction, image=tk_img)
+    label_image = Label(frame_prediction, image = tk_img)
     label_image.image = tk_img
     label_image.pack()
     if number_image == len(files)-1:
@@ -56,7 +56,7 @@ def nextImage(number_image):
         number_image+=1
     return label_image
 
-def openWindowPrediction():
+def openWindowPrediction(photo):
     test_path = path + "dataset/test/"
     files_abspath = []
     for file in files :
@@ -64,17 +64,17 @@ def openWindowPrediction():
 
     newWindow = Toplevel(root)
     newWindow.title('Predict Window')
-    #newWindow.geometry('{}x{}'.format(WINDOW_WIDTH,WINDOW_HEIGHT))
+    #newWindow.geometry('{}x{}'.format(WINDOW_WIDTH, WINDOW_HEIGHT))
     newWindow.geometry('1920x1010')
 
     frame_prediction = Frame(newWindow)
-    frame_prediction.pack(side=LEFT)
+    frame_prediction.pack(side = LEFT)
 
     number_image = 0
 
     img = Image.open(files_abspath[number_image])
     tk_img = ImageTk.PhotoImage(img)
-    label_image = Label(frame_prediction, image=tk_img)
+    label_image = Label(frame_prediction, image = tk_img)
     label_image.image = tk_img
     label_image.pack()
 
@@ -87,10 +87,10 @@ def openWindowPrediction():
     variable = StringVar(newWindow)
     variable.set(OptionList[0]) #Changer pour qu'il donne la valeur de la prédiction de l'algo
     opt = OptionMenu(newWindow, variable, *OptionList)
-    opt.pack(expand=YES)
+    opt.pack(expand = YES)
 
-    valid_button = Button(newWindow, text='Valid', command=lambda:[user_valid(),newWindow.destroy()])
-    valid_button.pack(expand=YES)
+    valid_button = Button(newWindow, text='Valid', command = lambda:[user_valid(), newWindow.destroy()])
+    valid_button.pack(expand = YES)
 
 
 def user_valid(preds, label_img):
@@ -98,7 +98,7 @@ def user_valid(preds, label_img):
     img_path = path + "dataset/test/" + label_img
     prediction = preds[label_img][0][0]
     train_path = path + "dataset/train/" + prediction
-    shutil.move(img_path,train_path)
+    shutil.move(img_path, train_path)
 
 def user_not_valid():
     ''''''
@@ -106,7 +106,7 @@ def user_not_valid():
 def displayImage(image, window):
     '''Display an image on the left side of a window'''
     img = Image.open(image)
-    canvas = Canvas(window, height=300, width=300)
+    canvas = Canvas(window, height = 300, width = 300)
     img = img.resize((300, 300), Image.BICUBIC)
     photo = ImageTk.PhotoImage(img)
     item = canvas.create_image(0, 0, image = photo)
@@ -130,53 +130,52 @@ def predictFiles():
             model_file = file
     dataset_path = path + "dataset/"
     test_path = path + "dataset/test/"
-    preds = testModelFunction(model_type=model_type, folder_path=test_path, model_path=os.path.abspath(model_file))
+    preds = testModelFunction(model_type = model_type, folder_path = test_path, model_path = os.path.abspath(model_file))
     return(preds)
     newWindow = Toplevel(root)
     newWindow.title('Predict Window')
-    newWindow.geometry('{}x{}'.format(WINDOW_WIDTH,WINDOW_HEIGHT))
-    img_full_path=[]
+    newWindow.geometry('{}x{}'.format(WINDOW_WIDTH, WINDOW_HEIGHT))
+    img_full_path = []
     for i in range(len(list(preds))):
-        img_full_path.append(test_path+list(preds)[i])
+        img_full_path.append(test_path + list(preds)[i])
 
     os.chdir(test_path)
     canvas = Canvas(newWindow, width = 300, height = 300)
     canvas.pack()
     img = ImageTk.PhotoImage(Image.open("C:/Users/geyma/Pictures/Fonds d'écran/Fond d'écran 1.jpg"))
-    canvas.create_image(20, 20, anchor=NW, image=img)
+    canvas.create_image(20, 20, anchor = NW, image = img)
 
     test = Label(newWindow,text='test')
     test.pack()
 
+    button5 = Button(newWindow, text = 'Valid', command = lambda:[user_valid(preds, list(preds)[0]), displayImage(test_path + list(preds)[1], newWindow), button5.destroy()])
+    button5.pack(side = RIGHT)
 
-    button5 = Button(newWindow, text='Valid', command=lambda:[user_valid(preds, list(preds)[0]), displayImage(test_path+list(preds)[1],newWindow),button5.destroy()])
-    button5.pack(side=RIGHT)
-
-    button6 = Button(newWindow, text='Valid', command=lambda:[user_valid(preds, list(preds)[1]), displayImage(test_path+list(preds)[2]),newWindow])
-    button6.pack(side=RIGHT)
+    button6 = Button(newWindow, text = 'Valid', command = lambda:[user_valid(preds, list(preds)[1]), displayImage(test_path + list(preds)[2]), newWindow])
+    button6.pack(side = RIGHT)
     button6.destroy()
 
-    button7 = Button(newWindow, text='Valid', command=user_valid(preds, list(preds)[2]))
-    button7.pack(side=RIGHT)
+    button7 = Button(newWindow, text = 'Valid', command = user_valid(preds, list(preds)[2]))
+    button7.pack(side = RIGHT)
     button7.destroy()
 
 
     for label_img in preds.keys():
         im = Image.open(label_img)
-        image = ImageTk.PhotoImage(im, master=newWindow)
+        image = ImageTk.PhotoImage(im, master = newWindow)
         dessin = tk.Canvas(fen, width = im.size[0], height = im.size[1])
         logo1 = dessin.create_image(0,0, anchor = tk.NW, image = logo)
         dessin.grid()
 
     #On demande à l'utilisateur de vérifier les prédictions
     for label_img in preds.keys():
-        text = Label(root, text=label_img)
+        text = Label(root, text = label_img)
         text.pack()
-        text = Label(root, text=preds[label_img])
+        text = Label(root, text = preds[label_img])
         text.pack()
-        button4 = Button(root, text='Valid', command=user_valid(preds, label_img))
+        button4 = Button(root, text = 'Valid', command = user_valid(preds, label_img))
         button4.pack()
-        button5 = Button(root, text='Not Valid')
+        button5 = Button(root, text = 'Not Valid')
         button5.pack()
     #On stocke le modèle que l'on vient d'utiliser dans les archives
     shutil.move(model_file, 'model_archives')
@@ -202,31 +201,31 @@ button2 = Button(root, text = 'Load files', command = lambda:[loadFiles(), predi
 button2.pack(expand = YES)
 
 """
-button3 = Button(root, text='Predict', command=lambda:[predict_images,openWindow])
-button3.pack(expand=YES)
+button3 = Button(root, text = 'Predict', command=lambda:[predict_images, openWindow])
+button3.pack(expand = YES)
 
 
 adhar = Label(
     frame1,
-    text='Upload Barley Image (png)'
+    text = 'Upload Barley Image (png)'
     )
-adhar.grid(row=0, column=0)
+adhar.grid(row = 0, column = 0)
 
 adharbtn = Button(
     frame1,
-    text ='Choose File',
+    text = 'Choose File',
     command = lambda:open_file()
     )
-adharbtn.grid(row=0,column=1)
+adharbtn.grid(row = 0, column = 1)
 
 frame1.grid()
 
 upld = Button(
     frame2,
-    text='Upload Files',
-    command=uploadFiles
+    text = 'Upload Files',
+    command = uploadFiles
     )
-upld.grid(row=1, column=0)
+upld.grid(row = 1, column = 0)
 
 frame2.grid()
 """
