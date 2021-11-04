51c05783bbdb92d56c6d2a2a8967bedcd0ad49c6
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2021-11-04 15:49:45 +0100

    Update API.py

--- ImageAI/API.py
+++ ImageAI/API.py
@@ -1,6 +1,6 @@
 ## Imports
 from tkinter import *
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilenames
 import time
 import os
 from PIL import Image, ImageTk
@@ -13,7 +13,7 @@ os.chdir(path)
 import shutil
 from core import trainModelFunction, testModelFunction, getModelType, readJson, createValidationFolders
 
-## Variables globales
+## Global variables
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 files = []
@@ -25,30 +25,36 @@ dataset_path = path + "dataset/"
 def chooseFiles():
     '''Open Windows Files Explorer and store the selected files into the list files'''
     FILETYPES = [("Images PNG",".png"),("Images JPG",".jpg")]
-    files_path = askopenfilename(title = "Select a file ...", filetypes=FILETYPES)
-    files.append(files_path)
-    text = Label(frame, text=os.path.basename(files_path))
-    text.pack()
+    files_path = askopenfilenames(title = "Select a file ...", filetypes=FILETYPES)
+    for i in range(len(files_path)):
+        files.append(files_path[i])
+        text = Label(frame, text=os.path.basename(files_path[i]))
+        text.pack()
 
 def loadFiles():
     '''Store the selected images into the test folder in the dataset folder'''
-    test_path = path + "dataset/test/"
-    for file in files :
-        shutil.move(file,test_path)
-    text = Label(frame, text='Files successfully loaded!')
-    text.pack()
+    try :
+        test_path = path + "dataset/test/"
+        for file in files :
+            shutil.move(file,test_path)
+        text = Label(frame, text='Files successfully loaded!')
+        text.pack()
+    except :
+        newWindow = Toplevel(root)
 
-def nextImage(number_image):
-    img = Image.open(files[number_image])
-    tk_img = ImageTk.PhotoImage(img)
-    label_image = Label(frame_prediction, image=tk_img)
-    label_image.image = tk_img
-    label_image.pack()
-    if number_image == len(files)-1:
-        number_image=0
-    else:
-        number_image+=1
-    return label_image
+        def center_window(newWindow):
+            eval = newWindow.nametowidget('.').eval
+            eval_('tk::PlaceWindow %s center' % newWindow)
+
+        center_window(newWindow)
+        text = Label(newWindow, text = 'Could not load images...')
+        text.pack(expand=YES)
+
+        quit_button = Button(newWindow, text = 'Exit', command=root.destroy)
+        quit_button.pack(expand=YES)
+
+        newWindow.wait_window()
+
 
 def openWindowTraining(model_file):
     '''Open a window for the training of the model. In this window, we can choose to retrain the model with all the training images (i.e. preivous training images + new images chosed in chooseFiles). We can also choose to exit the program.'''
@@ -102,7 +108,7 @@ def openWindowPrediction(preds):
     OptionList = [value for key, value in OptionDict.items()]
 
     variable = StringVar(newWindow)
-    variable.set(OptionList[0])
+    variable.set(preds[os.listdir(path + "dataset/test/")[0]][0][0])  # Variable is initially set to the prediction made by the model
     opt = OptionMenu(newWindow, variable, *OptionList)
     opt.pack(expand=YES)
 
@@ -120,17 +126,9 @@ def openWindowPrediction(preds):
 
     return(newWindow)
 
-def displayImage(image, window):
-    '''Display an image on the left side of a window'''
-    img = Image.open(image)
-    canvas = Canvas(window, height=300, width=300)
-    img = img.resize((300, 300), Image.BICUBIC)
-    photo = ImageTk.PhotoImage(img)
-    item = canvas.create_image(0,0, image = photo)
-    canvas.pack()
 
 def predictFiles():
-    '''On utilise l'algorithme pour prédire la classe d'évolution de l'orge des photos sélectionnées par choose_files. Le résultat dénommé 'preds' est un dictionnaire de la forme {'Nom_image' : ['prédiction', probabilité]}'''
+    '''We use the algorithm to predict the barley growth stage of the pictures selected by choose_files. The result named 'preds' is a dictionary of the form {'Image_name' : ['prediction', probability]}'''
     for file in os.listdir():
         if file.endswith(".h5"):
             model_file = file
@@ -140,67 +138,8 @@ def predictFiles():
         newWindow = openWindowPrediction(preds)
         newWindow.wait_window()
 
-    # Les images sont maintenant dans leurs folders respectifs dans train. On va maintenant créer une nouvelle fenêtre pour pouvoir entraîner le modèle sur toutes les données présentes dans le train
-
+    # All images are now in their respective folders in the train. We will now create a new window to train the model on all the data present in the train
     openWindowTraining(model_file)
-
-    """
-    for file in os.listdir():
-        if file.endswith(".h5"):
-            model_file = file
-    dataset_path = path + "dataset/"
-    test_path = path + "dataset/test/"
-    preds = testModelFunction(model_type=model_type, folder_path=test_path, model_path=os.path.abspath(model_file))
-    return(preds)
-    newWindow = Toplevel(root)
-    newWindow.title('Predict Window')
-    newWindow.geometry('{}x{}'.format(WINDOW_WIDTH,WINDOW_HEIGHT))
-    img_full_path=[]
-    for i in range(len(list(preds))):
-        img_full_path.append(test_path+list(preds)[i])
-
-    os.chdir(test_path)
-    canvas = Canvas(newWindow, width = 300, height = 300)
-    canvas.pack()
-    img = ImageTk.PhotoImage(Image.open("C:/Users/geyma/Pictures/Fonds d'écran/Fond d'écran 1.jpg"))
-    canvas.create_image(20, 20, anchor=NW, image=img)
-
-    test = Label(newWindow,text='test')
-    test.pack()
-
-
-    button5 = Button(newWindow, text='Valid', command=lambda:[user_valid(preds, list(preds)[0]), displayImage(test_path+list(preds)[1],newWindow),button5.destroy()])
-    button5.pack(side=RIGHT)
-
-    button6 = Button(newWindow, text='Valid', command=lambda:[user_valid(preds, list(preds)[1]), displayImage(test_path+list(preds)[2]),newWindow])
-    button6.pack(side=RIGHT)
-    button6.destroy()
-
-    button7 = Button(newWindow, text='Valid', command=user_valid(preds, list(preds)[2]))
-    button7.pack(side=RIGHT)
-    button7.destroy()
-
-
-    for label_img in preds.keys():
-        im = Image.open(label_img)
-        image = ImageTk.PhotoImage(im, master=newWindow)
-        dessin = tk.Canvas(fen, width = im.size[0], height = im.size[1])
-        logo1 = dessin.create_image(0,0, anchor = tk.NW, image = logo)
-        dessin.grid()
-
-    # On demande à l'utilisateur de vérifier les prédictions
-    for label_img in preds.keys():
-        text = Label(root, text=label_img)
-        text.pack()
-        text = Label(root, text=preds[label_img])
-        text.pack()
-        button4 = Button(root, text='Valid', command=user_valid(preds, label_img))
-        button4.pack()
-        button5 = Button(root, text='Not Valid')
-        button5.pack()
-    # On stocke le modèle que l'on vient d'utiliser dans les archives
-    shutil.move(model_file, 'model_archives')
-    """
 
 
 ## Root custom window
@@ -226,33 +165,4 @@ button1.pack()
 button2 = Button(root, text='Load files', command=lambda:[loadFiles(), predictFiles(), root.iconify()])
 button2.pack(expand=YES)
 
-"""
-button3 = Button(root, text='Predict', command=lambda:[predict_images,openWindow])
-button3.pack(expand=YES)
-
-
-adhar = Label(
-    frame1,
-    text='Upload Barley Image (png)'
-    )
-adhar.grid(row=0, column=0)
-
-adharbtn = Button(
-    frame1,
-    text ='Choose File',
-    command = lambda:open_file()
-    )
-adharbtn.grid(row=0,column=1)
-
-frame1.grid()
-
-upld = Button(
-    frame2,
-    text='Upload Files',
-    command=uploadFiles
-    )
-upld.grid(row=1, column=0)
-
-frame2.grid()
-"""
 root.mainloop()
